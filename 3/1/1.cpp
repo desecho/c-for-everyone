eaa5114be1d92135b606ac1b2ef0b91c325f3804
Anton Samarchyan <desecho@gmail.com>
2024-12-13 13:43:02 -0500

    Changes

--- 3/1/1.cpp
+++ 3/1/1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 const int N = 40; // Number of integers
 
 // Calculate the sum of integers
-inline void sum(int& p, int n, vector<int> d) {
+inline void sum(int& p, int n, const vector<int> d) {
   p = 0;
   for (int i = 0; i < n; ++i) {
     p = p + d[i];
